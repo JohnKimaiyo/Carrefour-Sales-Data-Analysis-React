bfa560a7a4921b69388e551f6f547fae0979301d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-23 12:12:00 +0300

    final commit

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div>
-      <h2>Amazon  Sales Business Requests</h2>
+      <h2>Carrefour Sales Analysis Business Requests</h2>
 
       <table>
         <tr>
@@ -20,7 +20,7 @@ export default function Header() {
         </tr>
         <tr>
           <td>Sales Representative</td>
-          <td>A detailed overview of Sales per Product</td>
+          <td>A detailed overview of Sales per Category of Products</td>
           <td>
             Can follow up my customers that buys the most and who we can sell
             ore to
@@ -31,23 +31,25 @@ export default function Header() {
         </tr>
         <tr>
           <td>Sales Representative</td>
-          <td>A detailed overview of Sales per Brand</td>
+          <td>A detailed overview of Sales per City</td>
           <td>
-            Can follow up my customers that buys the most and who we can sell
-            ore to
+            Can follow up my slaes agents which city buys the most and who we
+            can sell ore to
           </td>
           <td>
-            A Power BI dashboard which allows me to filter data for each Brand
+            A Power BI dashboard which allows me to filter data for each City
           </td>
         </tr>
         <tr>
           <td>Sales Representative</td>
-          <td>A detailed overview of Sales per Department</td>
-          <td>Can follow up my Department that sells the most</td>
+          <td>A detailed overview of Sales as per gender</td>
+          <td>
+            Can follow up my Department which Gender based prodcuts sells the
+            most
+          </td>
 
           <td>
-            A Power BI dashboard which allows me to filter data for each
-            Department
+            A Power BI dashboard which allows me to filter data for each Gender
           </td>
         </tr>
         <tr>
@@ -62,40 +64,12 @@ export default function Header() {
         </tr>
       </table>
 
-      <h2>View the Dashboard in this  Section</h2>
+      <h2>View the Dashboard in this Section</h2>
       <table>
         <tr>
           <th>Page Routes </th>
           <th>Description of the Pages</th>
           <th>URL Links</th>
-        </tr>
-        <tr>
-          <td>JPEG  Dashboard Data</td>
-          <td>Image file containing the Sales Visualised Data</td>
-
-          <td>
-            <a
-              href="https://github.com/JohnKimaiyo/Amazon-Data-Analys-React-App/blob/main/src/Publshed%20Report/Amazon%20Published%20Sales%20%20Summary.jpg"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View the Dashboard Here
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>Googlesheet Dashboard Data</td>
-          <td>Image containing the Sales Visualised Data in Googlesheet</td>
-
-          <td>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1NUMDmKWTlq3EncH-UNsKGHsMRyRDDuurPC5hVhrZT3o/edit#gid=0"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View the Dashboard Here
-            </a>
-          </td>
         </tr>
         <tr>
           <td>PDF Dashboard Data</td>
@@ -111,8 +85,49 @@ export default function Header() {
             </a>
           </td>
         </tr>
+
         <tr>
-          <td>SQL Queries</td>
+          <td>Googlesheet Dashboard Data</td>
+          <td>Image containing the Sales Visualised Data in Googlesheet</td>
+
+          <td>
+            <a
+              href="https://docs.google.com/spreadsheets/d/1NUMDmKWTlq3EncH-UNsKGHsMRyRDDuurPC5hVhrZT3o/edit#gid=0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View the Dashboard Here
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>Excel Dashboard </td>
+          <td>Check my Excel Dashboard</td>
+          <td>
+            <a
+              href="https://excel-to-json-javascript-converter.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click here to convert Excel data to JSON Data
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>Power BI File </td>
+          <td>Check out my Power BI Dashbord File</td>
+          <td>
+            <a
+              href="https://excel-to-json-javascript-converter.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click here to convert Excel data to JSON Data
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>SQL Scripts</td>
           <td>Checkout sql statement to clean up the data</td>
           <td>
             <a
@@ -125,8 +140,8 @@ export default function Header() {
           </td>
         </tr>
         <tr>
-          <td>Amazon Source Files </td>
-          <td>Amazon Kaggle Dataset</td>
+          <td> Carrefour Source Files </td>
+          <td> CarrefourKaggle Dataset</td>
           <td>
             <a
               href="https://www.kaggle.com/datasets/promptcloud/amazon-product-dataset-2020"
@@ -138,8 +153,8 @@ export default function Header() {
           </td>
         </tr>
         <tr>
-          <td>Amazon JSON Files </td>
-          <td>Amazon JSON Dataset</td>
+          <td> Carrefour JSON Files </td>
+          <td> Carrefour JSON Dataset</td>
           <td>
             <a
               href="https://raw.githubusercontent.com/JohnKimaiyo/Amazon-Data-Analys-React-App/main/src/JSON%20Files/amazon.json"
